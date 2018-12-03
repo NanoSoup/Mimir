0e1aa7b739b04e64ca50534127d79d2f0b3f8019
Jez Emery <jez.emery@strawberrysoup.co.uk>
2018-12-03 14:06:52 +0000

    Add Wordpress config settings

---
+++ scripts/composer/Wordpress.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Mimir;
+
+use Composer\Script\Event;
+
+class Wordpress
+{
+    public static function postProjectInstall(Event $event)
+    {
+        $vendorDir = $event->getComposer()->getConfig()->get('vendor-dir');
+        require $vendorDir . '/autoload.php';
+
+        $io = $event->getIO();
+        $io->write('Now to setup Wordpress');
+        $siteName = $io->ask('Site name: ');
+        $siteUrl = $io->ask('Site url: ');
+        $adminUser = $io->ask('Admin username: ');
+        $adminEmail = $io->ask('Admin email: ');
+
+        exec("wp core install --url='$siteUrl' --title='$siteName' --admin_user='$adminUser' --admin_email='$adminEmail'");
+
+    }
+}
